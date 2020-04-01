ba20ab883cb2950e09afac15002e27253eb2332e
hxd <houxiaodong_5@163.com>
2020-04-01 22:34:22 +0800

    第一次提交

---
+++ src/main/java/A.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Administrator on 2020/4/1/001.
+ */
+public class A {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
+
+
---
+++ src/main/java/B.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2020/4/1/001.
+ */
+public class B {
+    public static void main(String[] args) {
+        System.out.println("test b");
+    }
+}
